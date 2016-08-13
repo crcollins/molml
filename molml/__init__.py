71431bb68e70e3580c2f7d2062fe2d24343b30e6
Christopher Collins <crcollins@cmu.edu>
2016-08-13 19:50:03 -0400

    Add module description to __init__.py

---
+++ molml/__init__.py
@@ -0,0 +1,9 @@
+"""
+An interface between molecules and machine learning
+===================================================
+
+MolML is a python module to use to map molecules into representations that 
+are usable with machine learning. This is done using an API similar to 
+scikit-learn to keep things simple and straightforward. For documentation, 
+look at the docstrings.
+"""
