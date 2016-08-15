edef671f939283b5749a46c6440f34ab6dbee843
Christopher Collins <crcollins@cmu.edu>
2016-08-14 22:39:59 -0400

    Rename the qm7 example and refactor it

---
+++ examples/simple.py
@@ -0,0 +1,40 @@
+import numpy
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_absolute_error as MAE
+
+from molml.features import EncodedBond, BagOfBonds, Connectivity, CoulombMatrix
+
+from utils import load_qm7
+
+
+if __name__ == "__main__":
+    # This is just boiler plate code to load the data
+    Xin_train, Xin_test, y_train, y_test = load_qm7()
+
+    # Change this to make the tranformations parallel
+    # Values less than 1 will set to the number of cores the CPU has 
+    N_JOBS = 1
+
+    # Just a few examples of different features
+    tfs = [
+        EncodedBond(n_jobs=N_JOBS),
+        EncodedBond(spacing="inverse", n_jobs=N_JOBS),
+        BagOfBonds(n_jobs=N_JOBS),
+        CoulombMatrix(n_jobs=N_JOBS),
+        Connectivity(depth=1, n_jobs=N_JOBS),
+        Connectivity(depth=2, use_bond_order=True, n_jobs=N_JOBS),
+        Connectivity(depth=3, use_coordination=True, n_jobs=N_JOBS),
+    ]
+
+    for tf in tfs:
+        print tf
+        X_train = tf.fit_transform(Xin_train)
+        X_test = tf.transform(Xin_test)
+
+        # We will not do an hyperparmeter searching for simplicity
+        clf = Ridge()
+        clf.fit(X_train, y_train)
+        train_error = MAE(clf.predict(X_train), y_train)
+        test_error = MAE(clf.predict(X_test), y_test)
+        print "Train MAE: %.4f Test MAE: %.4f" % (train_error, test_error)
+        print 
---
+++ examples/utils.py
@@ -0,0 +1,92 @@
+import os
+import urllib2
+
+import numpy
+import scipy.io
+from scipy.constants import physical_constants, angstrom
+
+from molml.utils import NUM_TO_ELE
+
+
+BOHR_TO_ANGSTROMS = physical_constants['Bohr radius'][0] / angstrom
+
+
+def download_data():
+    '''
+    Download the QM7 data set
+    '''
+    url = "http://quantum-machine.org/data/qm7.mat"
+    print "Downloading data"
+    response = urllib2.urlopen(url)
+    print "Writing data"
+    with open("qm7.mat", "w") as f:
+        f.write(response.read())
+    print "Data written"
+
+
+def convert_input(Xin):
+    '''
+    Convert the QM7 data to the proper format
+
+    This removes all the padding values from the elements and coords
+    '''
+    new = []
+    for z, r in Xin:
+        temp = [(NUM_TO_ELE[int(x)], y) for x, y in zip(z, r) if x]
+        zs, rs = zip(*temp)
+        new.append((numpy.array(zs), numpy.array(rs)))
+    return new
+
+
+def get_fold_idxs(P, fold=0):
+    train_folds = [x for x in xrange(5) if x != fold]
+    train_idxs = numpy.ravel(P[train_folds])
+    test_idxs = numpy.ravel(P[fold])
+    return train_idxs, test_idxs
+
+
+def get_data_train_test(data, fold=0):
+    train_idxs, test_idxs = get_fold_idxs(data['P'], fold=fold)
+
+    y_train = data['T'][train_idxs]
+    y_test = data['T'][test_idxs]
+
+    R_train = data['R'][train_idxs]
+    Z_train = data['Z'][train_idxs]
+    Xin_train = zip(Z_train, R_train)
+    
+    R_test = data['R'][test_idxs]
+    Z_test = data['Z'][test_idxs]
+    Xin_test = zip(Z_test, R_test)
+    return Xin_train, Xin_test, y_train, y_test
+
+
+def load_qm7_data():
+    '''
+    Load the QM7 data set
+    '''
+    if not os.path.exists("qm7.mat"):
+        download_data()
+    data = scipy.io.loadmat("qm7.mat")
+
+    R = BOHR_TO_ANGSTROMS * data['R']
+    Xin = zip(data['Z'], R)
+    Xout = convert_input(Xin)
+    Z, R = zip(*Xout)
+    new_data = {
+        'P': data['P'],
+        'R': numpy.array(R),
+        'Z': numpy.array(Z),
+        'T': numpy.ravel(data['T']),
+    }
+    return new_data
+
+
+def load_qm7(fold=0):
+    data = load_qm7_data()
+    return get_data_train_test(data, fold=fold)
+
+
+
+
+
