c58b2dd49ad5c73d49b496025d13116da30b3b9a
Christopher Collins <crcollins@cmu.edu>
2017-01-01 19:39:29 -0500

    Change qm7 example to use MultiFeature

--- examples/qm7_long.py
+++ examples/qm7_long.py
@@ -1,9 +1,8 @@
-import numpy
 from sklearn.linear_model import Ridge
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_absolute_error as MAE
 
-from molml.features import EncodedBond, Connectivity
+from molml.features import EncodedBond, Connectivity, MultiFeature
 
 from utils import load_qm7
 
@@ -17,16 +16,10 @@ if __name__ == "__main__":
         Connectivity(depth=1, n_jobs=-1),
         Connectivity(depth=3, use_coordination=True, n_jobs=-1),
     ]
-    train_feats = []
-    test_feats = []
-    for tf in feats:
-        X_train = tf.fit_transform(Xin_train)
-        X_test = tf.transform(Xin_test)
-        train_feats.append(X_train)
-        test_feats.append(X_test)
 
-    X_train = numpy.hstack(train_feats)
-    X_test = numpy.hstack(test_feats)
+    full_feat = MultiFeature(feats)
+    X_train = full_feat.fit_transform(Xin_train)
+    X_test = full_feat.transform(Xin_test)
 
     clfs = [
         Ridge(alpha=0.01),
