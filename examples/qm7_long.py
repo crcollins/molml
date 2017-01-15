9ad141d4dbe26e2f30950fe9a6ee99493b8438f1
Christopher Collins <crcollins@cmu.edu>
2017-01-15 17:58:19 -0500

    Use sklearn FeatureUnion instead for qm7_long

--- examples/qm7_long.py
+++ examples/qm7_long.py
@@ -1,8 +1,9 @@
 from sklearn.linear_model import Ridge
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_absolute_error as MAE
+from sklearn.pipeline import FeatureUnion
 
-from molml.features import EncodedBond, Connectivity, MultiFeature
+from molml.features import EncodedBond, Connectivity
 
 from utils import load_qm7
 
@@ -12,12 +13,13 @@ if __name__ == "__main__":
     Xin_train, Xin_test, y_train, y_test = load_qm7()
 
     feats = [
-        EncodedBond(n_jobs=-1, max_depth=3),
-        Connectivity(depth=1, n_jobs=-1),
-        Connectivity(depth=3, use_coordination=True, n_jobs=-1),
+        ("encoded_bond", EncodedBond(n_jobs=-1, max_depth=3)),
+        ("atom_count", Connectivity(depth=1, n_jobs=-1)),
+        ("angle_count", Connectivity(depth=3, use_coordination=True,
+                                     n_jobs=-1)),
     ]
 
-    full_feat = MultiFeature(feats)
+    full_feat = FeatureUnion(feats)
     X_train = full_feat.fit_transform(Xin_train)
     X_test = full_feat.transform(Xin_test)
 
