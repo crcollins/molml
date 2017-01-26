5f11c3a4ba7d2dc5e8c9eb314b04348fbfe91ce3
Christopher Collins <crcollins@cmu.edu>
2017-01-26 10:16:58 -0500

    Add and update docstrings for the AtomKernel

--- molml/kernel.py
+++ molml/kernel.py
@@ -8,11 +8,16 @@ class AtomKernel(BaseFeature):
     '''
     Computes a kernel between molecules using atom similarity
 
+    This kernel comes with the benefit that because it is atom-wise, it stays
+    size consistent. So, for extensive properties this should properly scale
+    with the size of the molecule compared to other kernel methods.
+
     Parameters
     ----------
     input_type : string, default='list'
         Specifies the format the input values will be (must be one of 'list'
-        or 'filename').
+        or 'filename'). Note: This input type depends on the value from
+        transformer. See Below for more details.
 
     n_jobs : int, default=1
         Specifies the number of processes to create when generating the
@@ -25,7 +30,11 @@ class AtomKernel(BaseFeature):
     transformer : BaseFeature, default=None
         The transformer to use to convert molecules to atom-wise features. If
         this is not given, then it is assumed that the features have already
-        been created and will be passed directly to fit/transform.
+        been created and will be passed directly to fit/transform. Note: if no
+        transformer is given, then the assumed input type is going to be a
+        a list of (numbers, features) pairs. Where numbers is an iterable of
+        the atomic numbers, and features is a numpy array of the features
+        (shape=(n_atoms, n_features)).
 
     same_element : bool, default=True
         Require that the atom-atom similarity only be computed if the two
@@ -65,11 +74,12 @@ class AtomKernel(BaseFeature):
 
             symmetric : bool, default=True
                 Whether or not the kernel is symmetric. This is just to cut the
-                computational cost in half.
+                computational cost in half. This is mainly an optimization
+                when computing the (train, train) kernel.
 
         Returns
         -------
-            kernel : numpy.array, shape=(n_molecules_train, n_molecules_b)
+            kernel : numpy.array, shape=(n_molecules_b, n_molecules_fit)
                 The kernel matrix between the two sets of molecules
         '''
         kernel = numpy.zeros((len(b_feats), len(self._features)))
@@ -96,18 +106,77 @@ class AtomKernel(BaseFeature):
         return kernel
 
     def _para_get_numbers(self, X):
+        '''
+        Inner parallel function to collect the atomic numbers of a molecule
+
+        This is formulated in a way that it can easily be done in a map/reduce
+        fashion.
+
+        Parameters
+        ----------
+        X : object
+            An object to use for the fit.
+
+        Returns
+        -------
+        numbers : list
+            A list of the atomic numbers in the molecule.
+        '''
         data = self.convert_input(X)
         numbers = data.numbers
         return numbers
 
     def fit(self, X, y=None):
+        '''
+        Fit the model
+
+        If there is no self.transformer, then this assumes that the input is
+        a list of (features, numbers) pairs where features is a numpy array of
+        features (shape=(n_atoms, n_features)), and numbers is a list of
+        atomic numbers in the molecule.
+
+        Otherwise, it directly passes these values to the transformer to
+        compute the features, and extracts all the atomic numbers.
+
+        Parameters
+        ----------
+        X : list, shape=(n_samples, )
+            A list of objects to use to fit.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        '''
         if self.transformer is None:
             self._features, self._numbers = zip(*X)
         else:
             self._features = self.transformer.fit_transform(X, y)
             self._numbers = self.map(self._para_get_numbers, X)
+        return self
 
     def transform(self, X, y=None):
+        '''
+        Transform features/molecules into a kernel matrix
+
+        If there is no self.transformer, then this assumes that the input is
+        a list of (features, numbers) pairs where features is a numpy array of
+        features (shape=(n_atoms, n_features)), and numbers is a list of
+        atomic numbers in the molecule.
+
+        Otherwise, it directly passes these values to the transformer to
+        compute the features, and extracts all the atomic numbers.
+
+        Parameters
+        ----------
+        X : list, shape=(n_samples, )
+            A list of objects to use to transform
+
+        Returns
+        -------
+        kernel : array, shape=(n_samples, n_samples_fit)
+            The resulting kernel matrix
+        '''
         if self.transformer is None:
             features, numbers = zip(*X)
         else:
@@ -116,6 +185,22 @@ class AtomKernel(BaseFeature):
         return self.compute_kernel(features, numbers)
 
     def fit_transform(self, X, y=None):
+        '''
+        A slightly cheaper way of fitting and then transforming
+
+        This benefit comes from the resulting kernel matrix being symmetric.
+        Meaning, that only half of it has to be computed.
+
+        Parameters
+        ----------
+        X : list, shape=(n_samples, )
+            A list of objects to use to transform
+
+        Returns
+        -------
+        kernel : array, shape=(n_samples, n_samples)
+            The resulting kernel matrix
+        '''
         self.fit(X)
         return self.compute_kernel(self._features, self._numbers,
                                    symmetric=True)
