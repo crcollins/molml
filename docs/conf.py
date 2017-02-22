7cc385bbd21558f2b786b501bf92f6d1bde5cbc4
Christopher Collins <crcollins@cmu.edu>
2017-02-22 17:23:26 -0500

    Make doc conf.py generate API docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../molml'))
 
+from sphinx.apidoc import main
+main(['-F', '../molml', '-o', '.', '-e'])
 
 # -- General configuration ------------------------------------------------
 
