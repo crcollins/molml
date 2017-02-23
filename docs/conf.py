ef70b08fcb5ae19385fe64a94dc0fa81cd7ac68b
Christopher Collins <crcollins@cmu.edu>
2017-02-22 20:10:26 -0500

    Fix path information for docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../molml'))
+sys.path.insert(0, os.path.abspath('../'))
 
 from sphinx.apidoc import main
 main(['-F', '../molml', '-o', '.', '-e'])
