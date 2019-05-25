crcollins/molml

commit fd06dfbdd0164be9d29cdf409b02e92f5f497434

Cleanup stray ELE_TO_NUM conversion

molml/molecule.py
@@ -16,7 +16,7 @@ from .base import BaseFeature, SetMergeMixin, EncodedFeature
 from .utils import get_depth_threshold_mask_connections, get_coulomb_matrix
 from .utils import get_element_pairs, cosine_decay, needs_reversal
 from .utils import get_index_mapping, get_angles
-from .utils import get_graph_distance, ELE_TO_NUM
+from .utils import get_graph_distance
 from .constants import ELECTRONEGATIVITY, BOND_LENGTHS
 
 
@@ -1050,8 +1050,7 @@ class BagOfBonds(BaseFeature):
 
         bags = {key: [0 for i in range(value)]
                 for key, value in self._bag_sizes.items()}
-        numbers = [ELE_TO_NUM[x] for x in elements]
-        coulomb_matrix = get_coulomb_matrix(numbers, coords)
+        coulomb_matrix = get_coulomb_matrix(data.numbers, coords)
 
         ele_array = numpy.array(elements)
         for ele1, ele2 in bags.keys():

commit 5adb5e7d3ca113b966aeb60c8cc504304c6dd30d

Fix number sorting in BagOfBonds
This issue was caused by fd06dfb.

molml/molecule.py
@@ -1307,11 +1307,12 @@ class BagOfBonds(BaseFeature):
 
         data = self.convert_input(X)
         # Sort the elements and coords based on the element
-        temp = sorted(zip(data.elements, data.coords), key=lambda x: x[0])
-        elements, coords = zip(*temp)
+        temp = sorted(zip(data.elements, data.numbers, data.coords),
+                      key=lambda x: x[0])
+        elements, numbers, coords = zip(*temp)
 
         bags = {k: [0 for i in range(v)] for k, v in self._bag_sizes}
-        coulomb_matrix = get_coulomb_matrix(data.numbers, coords)
+        coulomb_matrix = get_coulomb_matrix(numbers, coords)
 
         ele_array = numpy.array(elements)
         for ele1, ele2 in bags.keys():